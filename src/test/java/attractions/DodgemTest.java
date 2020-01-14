cb0dd94e95685b8aa6b48b1c771b44939ebedb10
Talbekk <mt.campbell@outlook.com>
2020-01-14 17:25:29 +0000

    adds dodgem charge difference for adults and children

--- src/test/java/attractions/DodgemTest.java
+++ src/test/java/attractions/DodgemTest.java
@@ -2,16 +2,21 @@ package attractions;
 
 import org.junit.Before;
 import org.junit.Test;
+import people.Visitor;
 
 import static org.junit.Assert.assertEquals;
 
 public class DodgemTest {
 
     Dodgems dodgems;
+    Visitor visitor;
+    Visitor visitor1;
 
     @Before
     public void setUp() throws Exception {
         dodgems = new Dodgems("Bumper Cars", 5);
+        visitor1 = new Visitor(11, 1.2, 40.0);
+        visitor = new Visitor(30, 2.1, 120.0);
     }
 
 
@@ -28,6 +33,16 @@ public class DodgemTest {
     @Test
     public void hasVisitCount() {
         assertEquals(0, dodgems.getVisitCount());
+    }
+
+    @Test
+    public void canChargeAChildLessForTheDodgems(){
+        assertEquals(2.25, dodgems.priceFor(visitor1), 0.1);
+    }
+
+    @Test
+    public void canChargeRegularPriceForSomeoneOlderThanTwelve(){
+        assertEquals(4.50, dodgems.priceFor(visitor), 0.1);
     }
 
 }
