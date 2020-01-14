bdcd686e5faa8988d9006cfd6fc940652a88919f
Talbekk <mt.campbell@outlook.com>
2020-01-14 16:47:38 +0000

    adds restrictions to rollercoaster

--- src/main/java/attractions/RollerCoaster.java
+++ src/main/java/attractions/RollerCoaster.java
@@ -1,8 +1,16 @@
 package attractions;
 
-public class RollerCoaster extends Attraction {
+import behaviours.ISecurity;
+import people.Visitor;
+
+public class RollerCoaster extends Attraction implements ISecurity {
 
     public RollerCoaster(String name, int rating) {
         super(name, rating);
+    }
+
+    public boolean isAllowedTo(Visitor visitor) {
+        double height = visitor.getHeight();
+        return (visitor.getAge() > 12) && (height > 145.0);
     }
 }
--- src/test/java/attractions/RollercoasterTest.java
+++ src/test/java/attractions/RollercoasterTest.java
@@ -2,16 +2,22 @@ package attractions;
 
 import org.junit.Before;
 import org.junit.Test;
+import people.Visitor;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
 
 public class RollercoasterTest {
 
     RollerCoaster rollerCoaster;
+    Visitor visitor;
+    Visitor visitor1;
 
     @Before
     public void setUp() {
         rollerCoaster = new RollerCoaster("Blue Ridge", 10);
+        visitor = new Visitor(30, 2.1, 120.0);
+        visitor1 = new Visitor(15, 1.2, 40.);
     }
 
     @Test
@@ -28,4 +34,10 @@ public class RollercoasterTest {
     public void hasVisitCount() {
         assertEquals(0, rollerCoaster.getVisitCount());
     }
+
+    @Test
+    public void canFailEntryTest() { assertFalse(rollerCoaster.isAllowedTo(visitor)); }
+
+    @Test
+    public void canPassEntryTest() { assertFalse(rollerCoaster.isAllowedTo(visitor1)); }
 }
