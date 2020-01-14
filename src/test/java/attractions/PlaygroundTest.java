6d9c1d20162bd0a7ee32cfbec34d653c086ba33c
Talbekk <mt.campbell@outlook.com>
2020-01-14 16:37:40 +0000

    adds age restriction to tobacco stall trhough interface

--- src/test/java/attractions/PlaygroundTest.java
+++ src/test/java/attractions/PlaygroundTest.java
@@ -4,17 +4,18 @@ import org.junit.Before;
 import org.junit.Test;
 import people.Visitor;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.*;
 
 public class PlaygroundTest {
     Playground playground;
     Visitor visitor;
+    Visitor visitor1;
 
     @Before
     public void setUp() throws Exception {
         playground = new Playground("Fun Zone", 7);
         visitor = new Visitor(14, 1.2, 40.0);
+        visitor1 = new Visitor(30, 2.1, 120.0);
     }
 
     @Test
@@ -34,4 +35,7 @@ public class PlaygroundTest {
 
     @Test
     public void canPassAgeLimit(){ assertTrue(playground.isAllowedTo(visitor));}
+
+    @Test
+    public void canFailAgeLimit(){ assertFalse(playground.isAllowedTo(visitor1));}
 }
--- src/test/java/stalls/TobaccoStallTest.java
+++ src/test/java/stalls/TobaccoStallTest.java
@@ -2,16 +2,21 @@ package stalls;
 
 import org.junit.Before;
 import org.junit.Test;
+import people.Visitor;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.*;
 
 public class TobaccoStallTest {
 
     TobaccoStall tobaccoStall;
+    Visitor visitor;
+    Visitor visitor1;
 
     @Before
     public void setUp() throws Exception {
         tobaccoStall = new TobaccoStall("Jacks Drum", "Jack Jarvis", ParkingSpot.B1);
+        visitor = new Visitor(30, 2.1, 120.0);
+        visitor1 = new Visitor(14, 1.2, 40.);
     }
 
     @Test
@@ -28,4 +33,10 @@ public class TobaccoStallTest {
     public void hasParkingSpot() {
         assertEquals(ParkingSpot.B1, tobaccoStall.getParkingSpot());
     }
+
+    @Test
+    public void canPassAnAgeTest(){ assertTrue(tobaccoStall.isAllowedTo(visitor)); }
+
+    @Test
+    public void canFailAnAgeTest(){ assertFalse(tobaccoStall.isAllowedTo(visitor1)); }
 }
